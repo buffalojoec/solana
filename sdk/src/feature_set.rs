38611137ea9af9c9bff7b92b91f948aad98fa226
Joe <jcaulfield135@gmail.com>
2024-01-26 09:08:22 -0600

    test: migrate feature gate

--- sdk/src/feature_set.rs
+++ sdk/src/feature_set.rs
@@ -780,6 +780,10 @@ pub mod enable_chained_merkle_shreds {
     solana_sdk::declare_id!("7uZBkJXJ1HkuP6R3MJfZs7mLwymBcDbKdqbF51ZWLier");
 }
 
+pub mod programify_feature_gate {
+    solana_sdk::declare_id!("E4aWLLtPTir1mp2TBTEP8uGB3kJcZA453KbCD23Q7DnL");
+}
+
 lazy_static! {
     /// Map of feature identifiers to user-visible description
     pub static ref FEATURE_NAMES: HashMap<Pubkey, &'static str> = [
@@ -970,6 +974,7 @@ lazy_static! {
         (cost_model_requested_write_lock_cost::id(), "cost model uses number of requested write locks #34819"),
         (enable_gossip_duplicate_proof_ingestion::id(), "enable gossip duplicate proof ingestion #32963"),
         (enable_chained_merkle_shreds::id(), "Enable chained Merkle shreds #34916"),
+        (programify_feature_gate::id(), "[TEST]: Programify feature gate 01-26-2024"),
         /*************** ADD NEW FEATURES HERE ***************/
     ]
     .iter()
