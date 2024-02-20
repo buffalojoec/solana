cd1ffe2486881f4435393f0e388a51f2cb4924b5
Joe C <joe.caulfield@anza.xyz>
2024-02-20 14:53:29 -0600

    runtime: bank: add tests for overriding builtins

--- runtime/src/bank/tests.rs
+++ runtime/src/bank/tests.rs
@@ -4638,6 +4638,119 @@ fn test_add_builtin() {
 }
 
 #[test]
+fn test_override_builtins() {
+    let check_bank_builtins = |builtins: &[BuiltinPrototype]| {
+        let bank = Bank::new_for_tests_with_config(
+            &GenesisConfig::default(),
+            BankTestConfig {
+                override_builtins: Some(builtins),
+                ..BankTestConfig::default()
+            }
+        );
+        assert_eq!(
+            bank.builtin_programs.len(),
+            builtins.len(),
+        );
+        builtins
+            .iter()
+            .for_each(|builtin| {
+                assert!(bank.builtin_programs.get(&builtin.program_id).is_some());
+            });
+    };
+
+    check_bank_builtins(&[]);
+    check_bank_builtins(&[
+        BuiltinPrototype {
+            feature_id: None,
+            program_id: solana_system_program::id(),
+            name: "system_program",
+            entrypoint: solana_system_program::system_processor::Entrypoint::vm,
+        },
+    ]);
+    check_bank_builtins(&[
+        BuiltinPrototype {
+            feature_id: None,
+            program_id: solana_system_program::id(),
+            name: "system_program",
+            entrypoint: solana_system_program::system_processor::Entrypoint::vm,
+        },
+        BuiltinPrototype {
+            feature_id: None,
+            program_id: Pubkey::new_unique(),
+            name: "random_program",
+            entrypoint: solana_system_program::system_processor::Entrypoint::vm,
+        },
+    ]);
+}
+
+#[test]
+fn test_override_builtins_on_feature_activation() {
+    let check_bank_builtin_feature_activation = |builtins: &[BuiltinPrototype]| {
+        let feature_ids = builtins
+            .iter()
+            .filter_map(|builtin| builtin.feature_id)
+            .collect::<HashSet<_>>();
+
+        let mut bank = Bank::new_for_tests_with_config(
+            &GenesisConfig::default(),
+            BankTestConfig {
+                override_builtins: Some(builtins),
+                ..BankTestConfig::default()
+            }
+        );
+
+        let mut feature_set = FeatureSet::default();
+        feature_ids
+            .iter()
+            .for_each(|feature_id| {
+                feature_set.inactive.insert(*feature_id);
+                bank.store_account(feature_id, &feature::create_account(&Feature::default(), 42));
+            });
+        bank.feature_set = Arc::new(feature_set.clone());
+
+        // Assert the builtins have not been enabled yet.
+        assert!(bank.builtin_programs.is_empty());
+
+        bank.apply_feature_activations(ApplyFeatureActivationsCaller::NewFromParent, false, Some(builtins));
+
+        // Assert the builtins have been enabled.
+        assert_eq!(
+            bank.builtin_programs.len(),
+            builtins.len(),
+        );
+        builtins
+            .iter()
+            .for_each(|builtin| {
+                assert!(bank.builtin_programs.get(&builtin.program_id).is_some());
+            });
+    };
+
+    check_bank_builtin_feature_activation(&[]);
+    check_bank_builtin_feature_activation(&[
+        BuiltinPrototype {
+            feature_id: Some(Pubkey::new_unique()),
+            program_id: solana_system_program::id(),
+            name: "system_program",
+            entrypoint: solana_system_program::system_processor::Entrypoint::vm,
+        },
+    ]);
+    check_bank_builtin_feature_activation(&[
+        BuiltinPrototype {
+            feature_id: Some(Pubkey::new_unique()),
+            program_id: solana_system_program::id(),
+            name: "system_program",
+            entrypoint: solana_system_program::system_processor::Entrypoint::vm,
+        },
+        BuiltinPrototype {
+            feature_id: Some(Pubkey::new_unique()),
+            program_id: Pubkey::new_unique(),
+            name: "random_program",
+            entrypoint: solana_system_program::system_processor::Entrypoint::vm,
+        },
+    ]);
+}
+
+#[test]
 fn test_add_duplicate_static_program() {
     let GenesisConfigInfo {
         genesis_config,
