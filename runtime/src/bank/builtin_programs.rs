1ab5453578819d01d8a248c992d2f08292749f74
Joe C <joe.caulfield@anza.xyz>
2024-03-19 11:43:58 -0500

    runtime: add dynamic builtin tests

--- runtime/src/bank/builtin_programs.rs
+++ runtime/src/bank/builtin_programs.rs
@@ -1,11 +1,189 @@
 #[cfg(test)]
 mod tests {
     use {
-        crate::bank::*,
+        crate::bank::{
+            builtins::{BuiltinPrograms, BuiltinPrototype},
+            *,
+        },
         solana_sdk::{
-            ed25519_program, feature_set::FeatureSet, genesis_config::create_genesis_config,
+            ed25519_program, feature::Feature, feature_set::FeatureSet,
+            genesis_config::create_genesis_config,
         },
     };
+
+    #[test]
+    fn test_override_builtins_on_initialization() {
+        let check_bank_builtins = |builtins: Arc<BuiltinPrograms>| {
+            let bank = Bank::new_with_paths(
+                &GenesisConfig::default(),
+                Arc::<RuntimeConfig>::default(),
+                Vec::new(),
+                None,
+                Arc::clone(&builtins),
+                AccountSecondaryIndexes::default(),
+                AccountShrinkThreshold::default(),
+                false,
+                Some(ACCOUNTS_DB_CONFIG_FOR_TESTING),
+                None,
+                Some(Pubkey::new_unique()),
+                Arc::default(),
+            );
+
+            // Assert the bank's builtins contain all provided builtins.
+            builtins
+                .iter()
+                .filter(|b| b.enable_feature_id.is_none())
+                .for_each(|b| {
+                    assert!(bank.builtin_program_ids.get(&b.program_id).is_some());
+                });
+        };
+
+        check_bank_builtins(Arc::<BuiltinPrograms>::default());
+        check_bank_builtins(Arc::new(BuiltinPrograms::new(vec![])));
+        check_bank_builtins(Arc::new(BuiltinPrograms::new(vec![BuiltinPrototype {
+            enable_feature_id: None,
+            program_id: solana_system_program::id(),
+            name: "system_program",
+            entrypoint: solana_system_program::system_processor::Entrypoint::vm,
+        }])));
+        check_bank_builtins(Arc::new(BuiltinPrograms::new(vec![
+            BuiltinPrototype {
+                enable_feature_id: None,
+                program_id: solana_system_program::id(),
+                name: "system_program",
+                entrypoint: solana_system_program::system_processor::Entrypoint::vm,
+            },
+            BuiltinPrototype {
+                enable_feature_id: None,
+                program_id: Pubkey::new_unique(),
+                name: "random_program",
+                entrypoint: solana_system_program::system_processor::Entrypoint::vm,
+            },
+        ])));
+        check_bank_builtins(Arc::new(BuiltinPrograms::new(vec![
+            BuiltinPrototype {
+                enable_feature_id: None,
+                program_id: solana_system_program::id(),
+                name: "system_program",
+                entrypoint: solana_system_program::system_processor::Entrypoint::vm,
+            },
+            BuiltinPrototype {
+                enable_feature_id: None,
+                program_id: solana_stake_program::id(),
+                name: "stake_program",
+                entrypoint: solana_stake_program::stake_instruction::Entrypoint::vm,
+            },
+            BuiltinPrototype {
+                enable_feature_id: None,
+                program_id: Pubkey::new_unique(),
+                name: "random_program1",
+                entrypoint: solana_system_program::system_processor::Entrypoint::vm,
+            },
+            BuiltinPrototype {
+                enable_feature_id: None,
+                program_id: Pubkey::new_unique(),
+                name: "stake_program2",
+                entrypoint: solana_stake_program::stake_instruction::Entrypoint::vm,
+            },
+        ])));
+    }
+
+    #[test]
+    fn test_override_builtins_on_feature_activation() {
+        let check_bank_builtin_feature_activations = |builtins: Arc<BuiltinPrograms>| {
+            let mut bank = Bank::new_with_paths(
+                &GenesisConfig::default(),
+                Arc::<RuntimeConfig>::default(),
+                Vec::new(),
+                None,
+                Arc::clone(&builtins),
+                AccountSecondaryIndexes::default(),
+                AccountShrinkThreshold::default(),
+                false,
+                Some(ACCOUNTS_DB_CONFIG_FOR_TESTING),
+                None,
+                Some(Pubkey::new_unique()),
+                Arc::default(),
+            );
+
+            let mut feature_set = FeatureSet::default();
+            builtins
+                .iter()
+                .filter_map(|builtin| builtin.enable_feature_id)
+                .for_each(|feature_id| {
+                    feature_set.inactive.insert(feature_id);
+                    bank.store_account(
+                        &feature_id,
+                        &feature::create_account(&Feature::default(), 42),
+                    );
+                });
+            bank.feature_set = Arc::new(feature_set.clone());
+
+            // Assert the bank's builtins _do not_ contain the additional
+            // builtins, since they have not been enabled.
+            builtins.iter().for_each(|b| {
+                assert!(bank.builtin_program_ids.get(&b.program_id).is_none());
+            });
+
+            bank.apply_feature_activations(ApplyFeatureActivationsCaller::NewFromParent, false);
+
+            // Assert the bank's builtins contain the additional builtins,
+            // since they have now been enabled.
+            builtins.iter().for_each(|builtin| {
+                assert!(bank.builtin_program_ids.get(&builtin.program_id).is_some());
+            });
+        };
+
+        check_bank_builtin_feature_activations(Arc::new(BuiltinPrograms::new(vec![])));
+        check_bank_builtin_feature_activations(Arc::new(BuiltinPrograms::new(vec![
+            BuiltinPrototype {
+                enable_feature_id: Some(Pubkey::new_unique()),
+                program_id: solana_system_program::id(),
+                name: "system_program",
+                entrypoint: solana_system_program::system_processor::Entrypoint::vm,
+            },
+        ])));
+        check_bank_builtin_feature_activations(Arc::new(BuiltinPrograms::new(vec![
+            BuiltinPrototype {
+                enable_feature_id: Some(Pubkey::new_unique()),
+                program_id: solana_system_program::id(),
+                name: "system_program",
+                entrypoint: solana_system_program::system_processor::Entrypoint::vm,
+            },
+            BuiltinPrototype {
+                enable_feature_id: Some(Pubkey::new_unique()),
+                program_id: Pubkey::new_unique(),
+                name: "random_program",
+                entrypoint: solana_system_program::system_processor::Entrypoint::vm,
+            },
+        ])));
+        check_bank_builtin_feature_activations(Arc::new(BuiltinPrograms::new(vec![
+            BuiltinPrototype {
+                enable_feature_id: Some(Pubkey::new_unique()),
+                program_id: solana_system_program::id(),
+                name: "system_program",
+                entrypoint: solana_system_program::system_processor::Entrypoint::vm,
+            },
+            BuiltinPrototype {
+                enable_feature_id: Some(Pubkey::new_unique()),
+                program_id: solana_stake_program::id(),
+                name: "stake_program",
+                entrypoint: solana_stake_program::stake_instruction::Entrypoint::vm,
+            },
+            BuiltinPrototype {
+                enable_feature_id: Some(Pubkey::new_unique()),
+                program_id: Pubkey::new_unique(),
+                name: "random_program1",
+                entrypoint: solana_system_program::system_processor::Entrypoint::vm,
+            },
+            BuiltinPrototype {
+                enable_feature_id: Some(Pubkey::new_unique()),
+                program_id: Pubkey::new_unique(),
+                name: "stake_program2",
+                entrypoint: solana_stake_program::stake_instruction::Entrypoint::vm,
+            },
+        ])));
+    }
 
     #[test]
     fn test_apply_builtin_program_feature_transitions_for_new_epoch() {
