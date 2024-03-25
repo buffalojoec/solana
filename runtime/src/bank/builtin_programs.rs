eb6d68e24c64893c8b3ba7bca477697b7e080a6f
Joe C <joe.caulfield@anza.xyz>
2024-03-25 15:29:24 -0500

    runtime: bank: add feature activation tests for core bpf migration

--- runtime/src/bank/builtin_programs.rs
+++ runtime/src/bank/builtin_programs.rs
@@ -62,3 +62,224 @@ mod tests {
         bank.finish_init(&genesis_config, None, false);
     }
 }
+
+#[cfg(test)]
+mod tests_core_bpf_migration {
+    use {
+        crate::bank::{
+            builtins::{
+                core_bpf_migration::CoreBpfMigrationConfig, BuiltinPrototype,
+                StatelessBuiltinPrototype, BUILTINS, STATELESS_BUILTINS,
+            },
+            tests::create_simple_test_bank,
+            ApplyFeatureActivationsCaller, Bank,
+        },
+        solana_accounts_db::accounts_db::CalcAccountsHashDataSource,
+        solana_sdk::{
+            account::{AccountSharedData, ReadableAccount},
+            account_utils::StateMut,
+            bpf_loader_upgradeable::{self, get_program_data_address, UpgradeableLoaderState},
+            clock::{Slot, DEFAULT_SLOTS_PER_EPOCH},
+            feature::{self, Feature},
+            feature_set::FeatureSet,
+            pubkey::Pubkey,
+        },
+        std::sync::Arc,
+        test_case::test_case,
+    };
+
+    const PROGRAM_DATA_OFFSET: usize = UpgradeableLoaderState::size_of_programdata_metadata();
+
+    enum TestPrototype<'a> {
+        Builtin(&'a BuiltinPrototype),
+        Stateless(&'a StatelessBuiltinPrototype),
+    }
+    impl<'a> TestPrototype<'a> {
+        fn deconstruct(&'a self) -> (&'a Pubkey, &'a CoreBpfMigrationConfig) {
+            match self {
+                Self::Builtin(prototype) => (
+                    &prototype.program_id,
+                    prototype.core_bpf_migration_config.as_ref().unwrap(),
+                ),
+                Self::Stateless(prototype) => (
+                    &prototype.program_id,
+                    prototype.core_bpf_migration_config.as_ref().unwrap(),
+                ),
+            }
+        }
+    }
+
+    struct TestContext {
+        builtin_id: Pubkey,
+        source_program_id: Pubkey,
+        slot: Slot,
+        upgrade_authority_address: Option<Pubkey>,
+        elf: Vec<u8>,
+    }
+
+    impl TestContext {
+        // Initialize some test values and set up the source BPF upgradeable
+        // program in the bank.
+        fn new(
+            bank: &mut Bank,
+            builtin_id: &Pubkey,
+            feature_id: &Pubkey,
+            source_program_id: &Pubkey,
+        ) -> Self {
+            let slot = 99;
+            let upgrade_authority_address = Some(Pubkey::new_unique());
+            let elf = vec![4; 2000];
+
+            let source_program_data_address = get_program_data_address(source_program_id);
+
+            let source_program_account = AccountSharedData::new_data(
+                100_000_000,
+                &UpgradeableLoaderState::Program {
+                    programdata_address: source_program_data_address,
+                },
+                &bpf_loader_upgradeable::id(),
+            )
+            .unwrap();
+
+            let source_program_data_account = {
+                let mut data = bincode::serialize(&UpgradeableLoaderState::ProgramData {
+                    slot,
+                    upgrade_authority_address,
+                })
+                .unwrap();
+                data.extend_from_slice(&elf);
+
+                let mut account =
+                    AccountSharedData::new(100_000_000, data.len(), &bpf_loader_upgradeable::id());
+                account.set_data(data);
+                account
+            };
+
+            bank.store_account_and_update_capitalization(
+                source_program_id,
+                &source_program_account,
+            );
+            bank.store_account_and_update_capitalization(
+                &source_program_data_address,
+                &source_program_data_account,
+            );
+
+            let mut feature_set = FeatureSet::all_enabled();
+            feature_set.inactive.insert(*feature_id);
+            bank.feature_set = Arc::new(feature_set);
+
+            Self {
+                builtin_id: *builtin_id,
+                source_program_id: *source_program_id,
+                slot,
+                upgrade_authority_address,
+                elf,
+            }
+        }
+
+        // Evaluate the account state of the builtin post-migration.
+        // Ensure the builtin program account is now a BPF upgradeable program
+        // as well as the bank's builtins and cache have been updated.
+        fn run_program_checks_post_migration(&self, bank: &Bank) {
+            let program_account = bank.get_account(&self.builtin_id).unwrap();
+            let program_data_address = get_program_data_address(&self.builtin_id);
+
+            // Program account is owned by the upgradeable loader.
+            assert_eq!(program_account.owner(), &bpf_loader_upgradeable::id());
+
+            // Program account has the correct state, with a pointer to its program
+            // data address.
+            let program_account_state: UpgradeableLoaderState = program_account.state().unwrap();
+            assert_eq!(
+                program_account_state,
+                UpgradeableLoaderState::Program {
+                    programdata_address: program_data_address
+                }
+            );
+
+            let program_data_account = bank.get_account(&program_data_address).unwrap();
+
+            // Program data account is owned by the upgradeable loader.
+            assert_eq!(program_data_account.owner(), &bpf_loader_upgradeable::id());
+
+            // Program data account has the correct state.
+            // It should exactly match the original, including upgrade authority
+            // and slot.
+            let program_data_account_state_metadata: UpgradeableLoaderState =
+                bincode::deserialize(&program_data_account.data()[..PROGRAM_DATA_OFFSET]).unwrap();
+            assert_eq!(
+                program_data_account_state_metadata,
+                UpgradeableLoaderState::ProgramData {
+                    slot: self.slot,
+                    upgrade_authority_address: self.upgrade_authority_address
+                },
+            );
+            assert_eq!(
+                &program_data_account.data()[PROGRAM_DATA_OFFSET..],
+                &self.elf,
+            );
+
+            // The bank's builtins should no longer contain the builtin
+            // program ID.
+            assert!(!bank.builtin_program_ids.contains(&self.builtin_id));
+
+            // The cache should have unloaded both programs.
+            let program_cache = bank.transaction_processor.program_cache.read().unwrap();
+            assert!(!program_cache
+                .get_flattened_entries(true, true)
+                .iter()
+                .any(|(id, _)| id == &self.builtin_id || id == &self.source_program_id));
+        }
+    }
+
+    #[test_case(TestPrototype::Builtin(&BUILTINS[0]); "system")]
+    #[test_case(TestPrototype::Builtin(&BUILTINS[1]); "vote")]
+    #[test_case(TestPrototype::Builtin(&BUILTINS[2]); "stake")]
+    #[test_case(TestPrototype::Builtin(&BUILTINS[3]); "config")]
+    #[test_case(TestPrototype::Builtin(&BUILTINS[4]); "bpf_loader_deprecated")]
+    #[test_case(TestPrototype::Builtin(&BUILTINS[5]); "bpf_loader")]
+    #[test_case(TestPrototype::Builtin(&BUILTINS[6]); "bpf_loader_upgradeable")]
+    #[test_case(TestPrototype::Builtin(&BUILTINS[7]); "compute_budget")]
+    #[test_case(TestPrototype::Builtin(&BUILTINS[8]); "address_lookup_table")]
+    #[test_case(TestPrototype::Stateless(&STATELESS_BUILTINS[0]); "feature_gate")]
+    fn test(prototype: TestPrototype) {
+        let mut bank = create_simple_test_bank(0);
+
+        let (builtin_id, config) = prototype.deconstruct();
+        let feature_id = &config.feature_id;
+        let source_program_id = &config.source_program_id;
+
+        let test_context = TestContext::new(&mut bank, builtin_id, feature_id, source_program_id);
+
+        // Simulate crossing an epoch boundary for a new bank.
+        bank.apply_feature_activations(ApplyFeatureActivationsCaller::NewFromParent, false);
+
+        // Verify the feature was not activated and the program was not
+        // migrated.
+        assert!(!bank.feature_set.is_active(feature_id));
+        assert!(bank.get_account(source_program_id).is_some());
+
+        // Activate the feature.
+        bank.store_account_and_update_capitalization(
+            feature_id,
+            &feature::create_account(&Feature::default(), 42),
+        );
+
+        // Simulate crossing an epoch boundary for a new bank.
+        bank.apply_feature_activations(ApplyFeatureActivationsCaller::NewFromParent, false);
+
+        // Run the post-migration program checks.
+        assert!(bank.feature_set.is_active(feature_id));
+        test_context.run_program_checks_post_migration(&bank);
+
+        // Warp to another epoch and check again.
+        let collector_id = bank.collector_id;
+        let new_bank = Bank::warp_from_parent(
+            Arc::new(bank),
+            &collector_id,
+            DEFAULT_SLOTS_PER_EPOCH * 2,
+            CalcAccountsHashDataSource::IndexForTests,
+        );
+        test_context.run_program_checks_post_migration(&new_bank);
+    }
+}
--- runtime/src/bank/builtins/mod.rs
+++ runtime/src/bank/builtins/mod.rs
@@ -4,6 +4,7 @@ pub mod prototypes;
 pub use prototypes::{BuiltinPrototype, StatelessBuiltinPrototype};
 use solana_sdk::{bpf_loader, bpf_loader_deprecated, bpf_loader_upgradeable, feature_set};
 
+#[cfg(not(test))]
 pub static BUILTINS: &[BuiltinPrototype] = &[
     BuiltinPrototype {
         enable_feature_id: None,
@@ -84,8 +85,283 @@ pub static BUILTINS: &[BuiltinPrototype] = &[
     },
 ];
 
+#[cfg(not(test))]
 pub static STATELESS_BUILTINS: &[StatelessBuiltinPrototype] = &[StatelessBuiltinPrototype {
     core_bpf_migration_config: None,
     program_id: solana_sdk::feature::id(),
     name: "feature_gate_program",
 }];
+
+// Test-only
+#[cfg(test)]
+pub static BUILTINS: &[BuiltinPrototype] = &[
+    BuiltinPrototype {
+        enable_feature_id: None,
+        core_bpf_migration_config: Some(test_only::system_program::CONFIG),
+        program_id: solana_system_program::id(),
+        name: "system_program",
+        entrypoint: solana_system_program::system_processor::Entrypoint::vm,
+    },
+    BuiltinPrototype {
+        enable_feature_id: None,
+        core_bpf_migration_config: Some(test_only::vote_program::CONFIG),
+        program_id: solana_vote_program::id(),
+        name: "vote_program",
+        entrypoint: solana_vote_program::vote_processor::Entrypoint::vm,
+    },
+    BuiltinPrototype {
+        enable_feature_id: None,
+        core_bpf_migration_config: Some(test_only::stake_program::CONFIG),
+        program_id: solana_stake_program::id(),
+        name: "stake_program",
+        entrypoint: solana_stake_program::stake_instruction::Entrypoint::vm,
+    },
+    BuiltinPrototype {
+        enable_feature_id: None,
+        core_bpf_migration_config: Some(test_only::config_program::CONFIG),
+        program_id: solana_config_program::id(),
+        name: "config_program",
+        entrypoint: solana_config_program::config_processor::Entrypoint::vm,
+    },
+    BuiltinPrototype {
+        enable_feature_id: None,
+        core_bpf_migration_config: Some(test_only::solana_bpf_loader_deprecated_program::CONFIG),
+        program_id: bpf_loader_deprecated::id(),
+        name: "solana_bpf_loader_deprecated_program",
+        entrypoint: solana_bpf_loader_program::Entrypoint::vm,
+    },
+    BuiltinPrototype {
+        enable_feature_id: None,
+        core_bpf_migration_config: Some(test_only::solana_bpf_loader_program::CONFIG),
+        program_id: bpf_loader::id(),
+        name: "solana_bpf_loader_program",
+        entrypoint: solana_bpf_loader_program::Entrypoint::vm,
+    },
+    BuiltinPrototype {
+        enable_feature_id: None,
+        core_bpf_migration_config: Some(test_only::solana_bpf_loader_upgradeable_program::CONFIG),
+        program_id: bpf_loader_upgradeable::id(),
+        name: "solana_bpf_loader_upgradeable_program",
+        entrypoint: solana_bpf_loader_program::Entrypoint::vm,
+    },
+    BuiltinPrototype {
+        enable_feature_id: None,
+        core_bpf_migration_config: Some(test_only::compute_budget_program::CONFIG),
+        program_id: solana_sdk::compute_budget::id(),
+        name: "compute_budget_program",
+        entrypoint: solana_compute_budget_program::Entrypoint::vm,
+    },
+    BuiltinPrototype {
+        enable_feature_id: None,
+        core_bpf_migration_config: Some(test_only::address_lookup_table_program::CONFIG),
+        program_id: solana_sdk::address_lookup_table::program::id(),
+        name: "address_lookup_table_program",
+        entrypoint: solana_address_lookup_table_program::processor::Entrypoint::vm,
+    },
+    BuiltinPrototype {
+        enable_feature_id: Some(feature_set::zk_token_sdk_enabled::id()),
+        core_bpf_migration_config: Some(test_only::zk_token_proof_program::CONFIG),
+        program_id: solana_zk_token_sdk::zk_token_proof_program::id(),
+        name: "zk_token_proof_program",
+        entrypoint: solana_zk_token_proof_program::Entrypoint::vm,
+    },
+    BuiltinPrototype {
+        enable_feature_id: Some(feature_set::enable_program_runtime_v2_and_loader_v4::id()),
+        core_bpf_migration_config: Some(test_only::loader_v4::CONFIG),
+        program_id: solana_sdk::loader_v4::id(),
+        name: "loader_v4",
+        entrypoint: solana_loader_v4_program::Entrypoint::vm,
+    },
+];
+#[cfg(test)]
+pub static STATELESS_BUILTINS: &[StatelessBuiltinPrototype] = &[StatelessBuiltinPrototype {
+    core_bpf_migration_config: Some(test_only::feature_gate_program::CONFIG),
+    program_id: solana_sdk::feature::id(),
+    name: "feature_gate_program",
+}];
+
+// Configs used to test core BPF migration.
+#[cfg(test)]
+pub(crate) mod test_only {
+    use super::core_bpf_migration::{CoreBpfMigrationConfig, CoreBpfMigrationTargetType};
+    pub mod system_program {
+        use super::*;
+        pub mod feature {
+            solana_sdk::declare_id!("AnjsdWg7LXFbjDdy78wncCJs9PyTdWpKkFmHAwQU1mQ6");
+        }
+        pub mod source_program {
+            solana_sdk::declare_id!("EDEhzg1Jk79Wrk4mwpRa7txjgRxcE6igXwd6egFDVhuz");
+        }
+        pub const CONFIG: CoreBpfMigrationConfig = CoreBpfMigrationConfig {
+            source_program_id: source_program::id(),
+            feature_id: feature::id(),
+            migration_target: CoreBpfMigrationTargetType::Builtin,
+            datapoint_name: "core_bpf_migration_feature_gate_program",
+        };
+    }
+    pub mod vote_program {
+        use super::*;
+        pub mod feature {
+            solana_sdk::declare_id!("5wDLHMasPmtrcpfRZX67RVkBXBbSTQ9S4C8EJomD3yAk");
+        }
+        pub mod source_program {
+            solana_sdk::declare_id!("6T9s4PTcHnpq2AVAqoCbJd4FuHsdD99MjSUEbS7qb1tT");
+        }
+        pub const CONFIG: CoreBpfMigrationConfig = CoreBpfMigrationConfig {
+            source_program_id: source_program::id(),
+            feature_id: feature::id(),
+            migration_target: CoreBpfMigrationTargetType::Builtin,
+            datapoint_name: "core_bpf_migration_feature_gate_program",
+        };
+    }
+    pub mod stake_program {
+        use super::*;
+        pub mod feature {
+            solana_sdk::declare_id!("5gp5YKtNEirX45igBvp39bN6nEwhkNMRS7m2c63D1xPM");
+        }
+        pub mod source_program {
+            solana_sdk::declare_id!("2a3XnUr4Xfxd8hBST8wd4D3Qbiu339XKessYsDwabCED");
+        }
+        pub const CONFIG: CoreBpfMigrationConfig = CoreBpfMigrationConfig {
+            source_program_id: source_program::id(),
+            feature_id: feature::id(),
+            migration_target: CoreBpfMigrationTargetType::Builtin,
+            datapoint_name: "core_bpf_migration_feature_gate_program",
+        };
+    }
+    pub mod config_program {
+        use super::*;
+        pub mod feature {
+            solana_sdk::declare_id!("8Jve2vaMFhEgQ5JeB5HxekLx7hyjfRN2jLFawACqekNf");
+        }
+        pub mod source_program {
+            solana_sdk::declare_id!("73ALcNtVqyM3q7XsvB2xkVECvggu4CcLX5J2XKmpjdBU");
+        }
+        pub const CONFIG: CoreBpfMigrationConfig = CoreBpfMigrationConfig {
+            source_program_id: source_program::id(),
+            feature_id: feature::id(),
+            migration_target: CoreBpfMigrationTargetType::Builtin,
+            datapoint_name: "core_bpf_migration_feature_gate_program",
+        };
+    }
+    pub mod solana_bpf_loader_deprecated_program {
+        use super::*;
+        pub mod feature {
+            solana_sdk::declare_id!("8gpakCv5Pk5PZGv9RUjzdkk2GVQPGx12cNRUDMQ3bP86");
+        }
+        pub mod source_program {
+            solana_sdk::declare_id!("DveUYB5m9G3ce4zpV3fxg9pCNkvH1wDsyd8XberZ47JL");
+        }
+        pub const CONFIG: CoreBpfMigrationConfig = CoreBpfMigrationConfig {
+            source_program_id: source_program::id(),
+            feature_id: feature::id(),
+            migration_target: CoreBpfMigrationTargetType::Builtin,
+            datapoint_name: "core_bpf_migration_feature_gate_program",
+        };
+    }
+    pub mod solana_bpf_loader_program {
+        use super::*;
+        pub mod feature {
+            solana_sdk::declare_id!("8yEdUm4SaP1yNq2MczEVdrM48SucvZCTDSqjcAKfYfL6");
+        }
+        pub mod source_program {
+            solana_sdk::declare_id!("2EWMYGJPuGLW4TexLLEMeXP2BkB1PXEKBFb698yw6LhT");
+        }
+        pub const CONFIG: CoreBpfMigrationConfig = CoreBpfMigrationConfig {
+            source_program_id: source_program::id(),
+            feature_id: feature::id(),
+            migration_target: CoreBpfMigrationTargetType::Builtin,
+            datapoint_name: "core_bpf_migration_feature_gate_program",
+        };
+    }
+    pub mod solana_bpf_loader_upgradeable_program {
+        use super::*;
+        pub mod feature {
+            solana_sdk::declare_id!("oPQbVjgoQ7SaQmzZiiHW4xqHbh4BJqqrFhxEJZiMiwY");
+        }
+        pub mod source_program {
+            solana_sdk::declare_id!("6bTmA9iefD57GDoQ9wUjG8SeYkSpRw3EkKzxZCbhkavq");
+        }
+        pub const CONFIG: CoreBpfMigrationConfig = CoreBpfMigrationConfig {
+            source_program_id: source_program::id(),
+            feature_id: feature::id(),
+            migration_target: CoreBpfMigrationTargetType::Builtin,
+            datapoint_name: "core_bpf_migration_feature_gate_program",
+        };
+    }
+    pub mod compute_budget_program {
+        use super::*;
+        pub mod feature {
+            solana_sdk::declare_id!("D39vUspVfhjPVD7EtMJZrA5j1TSMp4LXfb43nxumGdHT");
+        }
+        pub mod source_program {
+            solana_sdk::declare_id!("KfX1oLpFC5CwmFeSgXrNcXaouKjFkPuSJ4UsKb3zKMX");
+        }
+        pub const CONFIG: CoreBpfMigrationConfig = CoreBpfMigrationConfig {
+            source_program_id: source_program::id(),
+            feature_id: feature::id(),
+            migration_target: CoreBpfMigrationTargetType::Builtin,
+            datapoint_name: "core_bpf_migration_feature_gate_program",
+        };
+    }
+    pub mod address_lookup_table_program {
+        use super::*;
+        pub mod feature {
+            solana_sdk::declare_id!("5G9xu4TnRShZpEhWyjAW2FnRNCwF85g5XKzSbQy4XpCq");
+        }
+        pub mod source_program {
+            solana_sdk::declare_id!("DQshE9LTac8eXjZTi8ApeuZJYH67UxTMUxaEGstC6mqJ");
+        }
+        pub const CONFIG: CoreBpfMigrationConfig = CoreBpfMigrationConfig {
+            source_program_id: source_program::id(),
+            feature_id: feature::id(),
+            migration_target: CoreBpfMigrationTargetType::Builtin,
+            datapoint_name: "core_bpf_migration_feature_gate_program",
+        };
+    }
+    pub mod zk_token_proof_program {
+        use super::*;
+        pub mod feature {
+            solana_sdk::declare_id!("GfeFwUzKP9NmaP5u4VfnFgEvQoeQc2wPgnBFrUZhpib5");
+        }
+        pub mod source_program {
+            solana_sdk::declare_id!("Ffe9gL8vXraBkiv3HqbLvBqY7i9V4qtZxjH83jYYDe1V");
+        }
+        pub const CONFIG: CoreBpfMigrationConfig = CoreBpfMigrationConfig {
+            source_program_id: source_program::id(),
+            feature_id: feature::id(),
+            migration_target: CoreBpfMigrationTargetType::Builtin,
+            datapoint_name: "core_bpf_migration_feature_gate_program",
+        };
+    }
+    pub mod loader_v4 {
+        use super::*;
+        pub mod feature {
+            solana_sdk::declare_id!("Cz5JthYp27KR3rwTCtVJhbRgwHCurbwcYX46D8setL22");
+        }
+        pub mod source_program {
+            solana_sdk::declare_id!("EH45pKy1kzjifB93wEJi91js3S4HETdsteywR7ZCNPn5");
+        }
+        pub const CONFIG: CoreBpfMigrationConfig = CoreBpfMigrationConfig {
+            source_program_id: source_program::id(),
+            feature_id: feature::id(),
+            migration_target: CoreBpfMigrationTargetType::Builtin,
+            datapoint_name: "core_bpf_migration_feature_gate_program",
+        };
+    }
+    pub mod feature_gate_program {
+        use super::*;
+        pub mod feature {
+            solana_sdk::declare_id!("8ZYAiJVVEon55wqGt2wZi5oVjxwK4cBUUfwMwHFvkqpB");
+        }
+        pub mod source_program {
+            solana_sdk::declare_id!("CWioXdq2ctv8Z4XdhmzJzgpU5i97ZHZZJVSJUmndV3mk");
+        }
+        pub const CONFIG: CoreBpfMigrationConfig = CoreBpfMigrationConfig {
+            source_program_id: source_program::id(),
+            feature_id: feature::id(),
+            migration_target: CoreBpfMigrationTargetType::Stateless,
+            datapoint_name: "core_bpf_migration_feature_gate_program",
+        };
+    }
+}
