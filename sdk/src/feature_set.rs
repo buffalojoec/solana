e87213ade9c3bab23e9e4c5d42e119fbbaad1684
Joe C <joe.caulfield@anza.xyz>
2024-03-29 16:45:41 -0500

    sdk: add feature gate for sol-get-epoch-stake

--- sdk/src/feature_set.rs
+++ sdk/src/feature_set.rs
@@ -801,6 +801,10 @@ pub mod abort_on_invalid_curve {
     solana_sdk::declare_id!("FuS3FPfJDKSNot99ECLXtp3rueq36hMNStJkPJwWodLh");
 }
 
+pub mod enable_syscall_get_epoch_stake {
+    solana_sdk::declare_id!("7mScTYkJXsbdrcwTQRs7oeCSXoJm4WjzBsRyf8bCU3Np");
+}
+
 lazy_static! {
     /// Map of feature identifiers to user-visible description
     pub static ref FEATURE_NAMES: HashMap<Pubkey, &'static str> = [
@@ -995,7 +999,8 @@ lazy_static! {
         (enable_tower_sync_ix::id(), "Enable tower sync vote instruction"),
         (chained_merkle_conflict_duplicate_proofs::id(), "generate duplicate proofs for chained merkle root conflicts"),
         (reward_full_priority_fee::id(), "Reward full priority fee to validators #34731"),
-        (abort_on_invalid_curve::id(), "Abort when elliptic curve syscalls invoked on invalid curve id SIMD-0137")
+        (abort_on_invalid_curve::id(), "Abort when elliptic curve syscalls invoked on invalid curve id SIMD-0137"),
+        (enable_syscall_get_epoch_stake::id(), "Enable syscall: sol_get_epoch_stake #884")
         /*************** ADD NEW FEATURES HERE ***************/
     ]
     .iter()
