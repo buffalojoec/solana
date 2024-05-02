b599894dd8095f42151c5fb1c0b331c096e4db7d
Joe Caulfield <joe.caulfield@anza.xyz>
2024-05-01 22:59:03 -0500

    program-runtime: invoke context: rename `programs_loaded_for_tx`

--- svm/src/message_processor.rs
+++ svm/src/message_processor.rs
@@ -241,8 +241,8 @@ mod tests {
         ];
         let mut transaction_context = TransactionContext::new(accounts, Rent::default(), 1, 3);
         let program_indices = vec![vec![2]];
-        let mut programs_loaded_for_tx_batch = ProgramCacheForTxBatch::default();
-        programs_loaded_for_tx_batch.replenish(
+        let mut program_cache_for_tx_batch = ProgramCacheForTxBatch::default();
+        program_cache_for_tx_batch.replenish(
             mock_system_program_id,
             Arc::new(ProgramCacheEntry::new_builtin(0, 0, MockBuiltin::vm)),
         );
@@ -282,10 +282,10 @@ mod tests {
         );
         let mut invoke_context = InvokeContext::new(
             &mut transaction_context,
+            &program_cache_for_tx_batch,
             environment_config,
             None,
             ComputeBudget::default(),
-            &programs_loaded_for_tx_batch,
             &mut programs_modified_by_tx,
         );
         let result = MessageProcessor::process_message(
@@ -336,10 +336,10 @@ mod tests {
         );
         let mut invoke_context = InvokeContext::new(
             &mut transaction_context,
+            &program_cache_for_tx_batch,
             environment_config,
             None,
             ComputeBudget::default(),
-            &programs_loaded_for_tx_batch,
             &mut programs_modified_by_tx,
         );
         let result = MessageProcessor::process_message(
@@ -380,10 +380,10 @@ mod tests {
         );
         let mut invoke_context = InvokeContext::new(
             &mut transaction_context,
+            &program_cache_for_tx_batch,
             environment_config,
             None,
             ComputeBudget::default(),
-            &programs_loaded_for_tx_batch,
             &mut programs_modified_by_tx,
         );
         let result = MessageProcessor::process_message(
@@ -476,8 +476,8 @@ mod tests {
         ];
         let mut transaction_context = TransactionContext::new(accounts, Rent::default(), 1, 3);
         let program_indices = vec![vec![2]];
-        let mut programs_loaded_for_tx_batch = ProgramCacheForTxBatch::default();
-        programs_loaded_for_tx_batch.replenish(
+        let mut program_cache_for_tx_batch = ProgramCacheForTxBatch::default();
+        program_cache_for_tx_batch.replenish(
             mock_program_id,
             Arc::new(ProgramCacheEntry::new_builtin(0, 0, MockBuiltin::vm)),
         );
@@ -515,10 +515,10 @@ mod tests {
         );
         let mut invoke_context = InvokeContext::new(
             &mut transaction_context,
+            &program_cache_for_tx_batch,
             environment_config,
             None,
             ComputeBudget::default(),
-            &programs_loaded_for_tx_batch,
             &mut programs_modified_by_tx,
         );
         let result = MessageProcessor::process_message(
@@ -554,10 +554,10 @@ mod tests {
         );
         let mut invoke_context = InvokeContext::new(
             &mut transaction_context,
+            &program_cache_for_tx_batch,
             environment_config,
             None,
             ComputeBudget::default(),
-            &programs_loaded_for_tx_batch,
             &mut programs_modified_by_tx,
         );
         let result = MessageProcessor::process_message(
@@ -590,10 +590,10 @@ mod tests {
         );
         let mut invoke_context = InvokeContext::new(
             &mut transaction_context,
+            &program_cache_for_tx_batch,
             environment_config,
             None,
             ComputeBudget::default(),
-            &programs_loaded_for_tx_batch,
             &mut programs_modified_by_tx,
         );
         let result = MessageProcessor::process_message(
@@ -673,8 +673,8 @@ mod tests {
             Some(transaction_context.get_key_of_account_at_index(0).unwrap()),
         ));
         let sysvar_cache = SysvarCache::default();
-        let mut programs_loaded_for_tx_batch = ProgramCacheForTxBatch::default();
-        programs_loaded_for_tx_batch.replenish(
+        let mut program_cache_for_tx_batch = ProgramCacheForTxBatch::default();
+        program_cache_for_tx_batch.replenish(
             mock_program_id,
             Arc::new(ProgramCacheEntry::new_builtin(0, 0, MockBuiltin::vm)),
         );
@@ -687,10 +687,10 @@ mod tests {
         );
         let mut invoke_context = InvokeContext::new(
             &mut transaction_context,
+            &program_cache_for_tx_batch,
             environment_config,
             None,
             ComputeBudget::default(),
-            &programs_loaded_for_tx_batch,
             &mut programs_modified_by_tx,
         );
         let result = MessageProcessor::process_message(
