946bc7954d62e9b99f0c16f45aab20bc9a3ccf6c
Joe Caulfield <joe.caulfield@anza.xyz>
2024-05-04 14:25:39 -0500

    syscall: get-epoch-stake: tests

--- program-runtime/src/invoke_context.rs
+++ program-runtime/src/invoke_context.rs
@@ -39,7 +39,7 @@ use {
     solana_vote::vote_account::VoteAccountsHashMap,
     std::{
         alloc::Layout,
-        cell::RefCell,
+        cell::{Ref, RefCell},
         fmt::{self, Debug},
         rc::Rc,
         sync::{atomic::Ordering, Arc},
@@ -599,6 +599,11 @@ impl<'a> InvokeContext<'a> {
     /// Get this invocation's compute budget
     pub fn get_compute_budget(&self) -> &ComputeBudget {
         &self.compute_budget
+    }
+
+    /// Get this invocation's compute meter.
+    pub fn get_compute_meter(&self) -> Ref<u64> {
+        self.compute_meter.borrow()
     }
 
     /// Get the current feature set.
