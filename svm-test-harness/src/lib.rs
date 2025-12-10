4a18228b7449486491003a1fee51d2671bfc8375
Joseph Caulfield <jcaulfield135@gmail.com>
2025-12-10 06:52:15 +0000

    svm-test-harness: add syscalls crate

--- svm-test-harness/src/lib.rs
+++ svm-test-harness/src/lib.rs
@@ -1,3 +1,6 @@
 //! Solana SVM test harness.
 
-pub use {solana_svm_test_harness_fixture as fixture, solana_svm_test_harness_instr as instr};
+pub use {
+    solana_svm_test_harness_fixture as fixture, solana_svm_test_harness_instr as instr,
+    solana_svm_test_harness_syscall as syscall,
+};
---
+++ svm-test-harness/syscall/src/lib.rs
@@ -0,0 +1,1 @@
+//! Solana SVM test harness for syscalls.
