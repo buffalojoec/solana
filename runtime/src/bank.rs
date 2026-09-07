1eaad15fa38e69916c69b8c07944af979535261f
Joe Caulfield <jcaulfield135@gmail.com>
2026-09-07 06:06:05 +0000

    runtime: expose the batch processor

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -7365,6 +7365,10 @@ impl Bank {
     pub fn slot_time_reduction_active(&self) -> bool {
         self.ns_per_slot != self.slot_params.baseline_params().ns_per_slot()
     }
+
+    pub fn transaction_processor(&self) -> &TransactionBatchProcessor<BankForks> {
+        &self.transaction_processor
+    }
 }
 
 /// Returns a thread pool intended to be used for reward calculation. This
